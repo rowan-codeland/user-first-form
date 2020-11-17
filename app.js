f6f434f59205b20c57131472cf7db7d1690bc71e
Rowan <rowan.copeland@gmail.com>
2020-11-17 18:48:47 +0000

    add active class to each question & JS to edit on click

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 let buttons = document.getElementsByClassName("formBtn")
+
 for (let i = 0, l = buttons.length; i < l; i++) {
   buttons[i].addEventListener('click', function() {
     console.log("Hello World")
   })
+}
+
+let questionContainer = document.getElementsByClassName("field");
+
+for (var i = 0; i < questionContainer.length; i++) {
+  questionContainer[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
